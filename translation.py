d9a814eb02ea467c98425e7b9c4f857295723f88
XHackerzkali <64786928+XHackerzkali@users.noreply.github.com>
2020-07-09 17:23:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -35,7 +35,7 @@ URL | filename | username | password"""
 --------
 Telegram ID: <code>{}</code>
 Plan name: Free 
-Expires on: Never Thank @XHacketzKali 😎"""
+Expires on: Never Thank @XHackerzkali 😎"""
     HELP_USER = """Hai am URL Uploader bot..
     
 1. Send url (Link|New Name with Extension).
